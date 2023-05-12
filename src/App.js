ecee253c0b02f37b5c9358f53d2c75794df13424
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-05-12 22:57:04 +0200

    first part almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './style.css';
+
+import star from './images/icon-star.svg';
+
+function Icon(){
+  return(
+    <img src={star} alt="" className="main-icon"></img>
+  );
+}
+
+function Texts(){
+  return(
+    <div>
+      <p className="texts-title">How did we do?</p>
+      <p className="texts-desc">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+    </div>
+  );
+}
+
+function Buttons(){
+
+  let numbers = [];
+
+  for(var i = 0; i <= 4; i++){
+    numbers[i] = i + 1;
+  }
+
+  const buttons = numbers.map((number) =>
+    <div id={number.toString()} className="flex flex-justcont-c flex-alignit-c button-colours buttons-each">
+      <div key={number.toString()}>{number}</div>
+    </div>
+  );
+
+  return(
+    <div class="flex flex-justcont-sa buttons">
+      {buttons}
+    </div>
+  )
+}
+
+function Submit(){
+  return(
+    <button className="submit">
+      SUBMIT
+    </button>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-justcont-c flex-alignit-c main">
+      <div className="main-block">
+        <div className='main-content'>
+          <Icon />
+          <Texts />
+          <Buttons />
+          <Submit />
+        </div>
+      </div>
     </div>
   );
 }
